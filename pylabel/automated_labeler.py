3fbd61e84825fc37d7d2fc2db07e717b7211f4b9
Aika Aldayarova <aika.aldayar@gmail.com>
2025-04-15 16:52:56 -0400

    built extract_bluesky_image

--- pylabel/automated_labeler.py
+++ pylabel/automated_labeler.py
@@ -9,6 +9,10 @@ from io import BytesIO
 from PIL import Image
 import requests
 
+# Imports for Milestone 4
+from perception import hashers
+from .label import did_from_handle
+
 T_AND_S_LABEL = "t-and-s"
 DOG_LABEL = "dog"
 THRESH = 0.3
@@ -36,17 +40,81 @@ class AutomatedLabeler:
         # Milestone 4
         self.dog_hashes = []
         dog_images_dir = os.path.join(input_dir, DOG_DIR)
+        hasher = hashers.PHash()
+    
         for file in os.listdir(dog_images_dir):
             if not file.lower().endswith(('.jpg', '.jpeg', '.png')):
                 continue
             dogImage = os.path.join(dog_images_dir, file)
             try:
                 with Image.open(dogImage) as img:
+
                     dogHash = None # makeHash(img)
                     self.dogImageHashes.append(dogHash)
             except Exception as e:
                 print(f"Error processing dog image {dogImage}: {e}")
+    
+    # Helper function for Milestone 4 to extract image from Bluesky URL
+    def extract_bluesky_image (self):
+        client = Client()
+        # path = "../test-data/input-posts-dogs.csv"
+        path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "test-data", "input-posts-dogs.csv")
 
+        # path = os.path.join(self.input_dir, "")
+
+        try:
+            with open(path, newline="", encoding="utf-8") as file:
+                file = csv.reader(file)
+                next(file)
+                for line in file:
+                    if not line: # skips any missing/empty lines (if any)
+                        continue
+                    value = line[0].strip().lower()
+                    print(f"The line is: {line}")
+
+                    # Instead of using helper function in label.py, create own simpler helper function
+                    def parse_post_url(url):
+                        parts = url.strip('/').split('/')
+                        handle = parts[-3]
+                        rkey = parts[-1]
+                        return handle, rkey
+
+                    handle, rkey = parse_post_url(value)
+                    # print(f"post_from_url returned handle:{handle}, rkey: {rkey}")
+
+                    # Resolving the handle to DID using helper function in label.py
+                    did = did_from_handle(handle)
+
+                    # Getting the post record (?)
+                    post_record = client.com.atproto.repo.get_record({
+                        'repo': did,
+                        'collection': 'app.bsky.feed.post',
+                        'rkey': rkey
+                    })
+                    print(f"post_record is {post_record}")
+
+                    # Getting the post embedding 
+                    embed = post_record['value'].embed
+                    images = []
+                    if embed and hasattr(embed, "images"):
+                        for image_data in embed.images:
+                            cid = image_data.image.ref.link 
+                            images.append(cid)
+                    else:
+                        print("No images found in this post.")
+
+                    # Extracting image
+                    for cid in images:
+                        img_url = f"https://bsky.social/xrpc/com.atproto.sync.getBlob?did={did}&cid={cid}"
+                        response = requests.get(img_url)
+                        image = Image.open(BytesIO(response.content))
+                        image.show()
+
+        except Exception as e:
+            print(f"Error reading file {path}: {e}")
+
+        return None
+    
     # Milestone 2
     def get_csv(self, path: str) -> List[str]:
         """
@@ -118,3 +186,8 @@ class AutomatedLabeler:
                 labels.add(source)
 
         return list(labels)
+
+if __name__ == "__main__":
+    client = Client()
+    labeler_obj = AutomatedLabeler(client, "./labeler-inputs")
+    print(labeler_obj.extract_bluesky_image())
